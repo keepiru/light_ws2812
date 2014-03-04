0b59dbb75a8996742f76b9acae71d0ea1d6ddf76
Kai <github@neofin.net>
2014-03-03 21:03:58 -0800

    Here's what I did yesterday
     * added flash: target to use my AVR programmer
     * changed chip to mega168
     * added some color swipes
     * added support for up to 10 leds

--- light_ws2812_AVR/Examples/RGB_blinky.c
+++ light_ws2812_AVR/Examples/RGB_blinky.c
@@ -12,7 +12,7 @@
 #include <avr/interrupt.h>
 #include "light_ws2812.h"
 
-struct cRGB led[1];
+struct cRGB led[10];
 
 int main(void)
 {
@@ -25,11 +25,90 @@ int main(void)
   CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
   #endif
 
+  uint64_t i, j;
+
+  for (i=0; i<255; i++) {
+    led[0].r = led[0].g = led[0].b = i;
+    led[1].r = led[1].g = led[1].b = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for (i=255; i>0; i--) {
+    led[0].r = led[0].g = led[0].b = i;
+    led[1].r = led[1].g = led[1].b = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for (i=0; i<255; i++) {
+    led[0].r = i;
+    led[1].r = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for (i=255; i>0; i--) {
+    led[0].r = i;
+    led[1].r = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for (i=0; i<255; i++) {
+    led[0].r = led[0].b = i;
+    led[1].r = led[1].b = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for (i=255; i>0; i--) {
+    led[0].r = led[0].b = i;
+    led[1].r = led[1].b = i;
+    ws2812_setleds(led,3);
+    _delay_ms(3);
+  }
+
+  for(i=0; i<100; i++) {
+    led[0].r = rand() % 256;
+    led[0].g = rand() % 256;
+    led[0].b = rand() % 256;
+    led[1].r = rand() % 256;
+    led[1].g = rand() % 256;
+    led[1].b = rand() % 256;
+    ws2812_setleds(led,3);
+    _delay_ms(30);
+  }
+
+  while(1){
+    led[0].r = 255 * rand() % 2;
+    led[0].g = 255 * rand() % 2;
+    led[0].b = 255 * rand() % 2;
+    led[1].r = 255 * rand() % 2;
+    led[1].g = 255 * rand() % 2;
+    led[1].b = 255 * rand() % 2;
+    ws2812_setleds(led,3);
+    _delay_ms(30);
+  }
+
+  for(i=0; i<10000; i++) {
+    j = 1ULL << (i % 48);
+    led[0].r = ( j          ) ;
+    led[0].g = ( j     >> 4 ) ;
+    led[0].b = ( j     >> 8 ) ;
+    led[1].r = ( j / 8 >> 0 ) ;
+    led[1].g = ( j / 8 >> 8 ) ;
+    led[1].b = ( j / 8 >> 16) ;
+    ws2812_setleds(led,3);
+    _delay_ms(10);
+  }
+
+
 
   while(1)
   {
     led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
-    ws2812_setleds(led,1);
+    ws2812_setleds(led,2);
     _delay_ms(500);                         // wait for 500ms.
 
     led[0].r=0;led[0].g=255;led[0].b=0;			// green
